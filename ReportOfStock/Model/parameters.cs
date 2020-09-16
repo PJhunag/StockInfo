dc89560475f7b3c50fa7d410894b272ab3c8fbc3
Tree <ka0132@msn.com>
2020-09-16 11:03:25 +0800

    新增報表產生
    新增報表產生

---
+++ ReportOfStock/Model/parameters.cs
@@ -0,0 +1,361 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+public class parameters {
+
+    //先定義好相關變數
+    public string Name = ""; //名稱
+    public string? Type = ""; //類別
+    public double FiveYearsAvgerageEPS = new double (); //五年平均EPS
+
+    public string LastFourEPS = ""; //近四季平均EPS
+    public double LastMoneyDividend = new double (); //去年股利(現金)
+    public double LastStockDividend = new double (); //去年股利(股票)
+    public double FiveYearsAvgerageDividend = new double (); //五年平均殖利率
+    public string FiveYearsAvgeragePrice = ""; //五年均價
+    public double Price = new double (); //目前價格
+    public string UpdateDate = ""; //價格更新日
+    public double ThisYearAccumulationProfit = new double (); //當年淨利
+    public double ThisYearAccumulationOperatingIncome = new double (); //當年營收
+    public double LastYearAccumulationProfit = new double (); //去年淨利
+    public double LastYearAccumulationOperatingIncome = new double (); //去年營收
+    public double LastEPS = new double (); //去年EPS
+    public double RevenueIncreaseRatio = new double (); //累積年增率(營收增加比率)
+    public double StockIncreaseRatio = new double (); //股票(股數)增加比率
+
+    public double EPSUseRatio = new double (); //EPS發放率
+
+    public string OperatingUpdateTime = ""; //營收更新年月
+    public string no;
+    MySqlConnection conn;
+    string StrSQL;
+    public parameters (string stockNo, MySqlConnection dbconn) {
+        no = stockNo;
+        conn = dbconn;
+
+        int year = DateTime.Now.Year; //當年年度
+
+        //取得股票名稱
+        getYearName ();
+
+        //取得當年營收AND淨利
+        getYearAccumulationOperatingIncomeAndYearAccumulationProfit (year, "this");
+
+        //取得去年營收AND淨利
+        getYearAccumulationOperatingIncomeAndYearAccumulationProfit (year - 1, "last");
+
+        //取得去年EPS(發放年度為今年)
+        getLastYearEPSAndEPSUseRatioAndStockIncreaseRatio (year);
+
+        //取得營收成長率(本年最新)
+        getRevenueIncreaseRatio (year);
+
+        //抓取平均EPS(五年) & 平均殖利率(五年)
+        getFiveYearsAvgerageEPSandDividend (year);
+
+        //抓取時價/更新日
+        getPriceAndUpdateDate ();
+
+        //抓取近四季EPS
+        getLastFourEPS ();
+
+        //抓取營收最後更新時間
+        getOperatingUpdateTime ();
+
+        //抓取五年股票均價
+        getFiveYearsAvgeragePrice ();
+
+        //抓取股票類別
+        getType ();
+
+    }
+
+    public void getYearName () {
+        try {
+
+            StrSQL = "select name003 from name_t where name001 = ?name001 and name002 = ?name002 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@name001", no);
+            myCmd.Parameters.AddWithValue ("@name002", "zh_TW");
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+
+                Name = reader.GetString (0); //名稱-name003
+
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+
+    //取得指定年份營收AND淨利
+    public void getYearAccumulationOperatingIncomeAndYearAccumulationProfit (int year, string type) {
+        try {
+
+            StrSQL = "select sum(prft004),sum(prft008) from prft_t where prft001 = ?prft001 and prft002 = ?prft002 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@prft001", no);
+            myCmd.Parameters.AddWithValue ("@prft002", year);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                if (type == "this") {
+                    ThisYearAccumulationOperatingIncome = Double.Parse (reader.GetString (0)); //營收-prft004
+                    ThisYearAccumulationProfit = Double.Parse (reader.GetString (1)); //純利-prft008
+                } else {
+                    LastYearAccumulationOperatingIncome = Double.Parse (reader.GetString (0)); //營收-prft004
+                    LastYearAccumulationProfit = Double.Parse (reader.GetString (1)); //純利-prft008
+                }
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+
+    public void getLastYearEPSAndEPSUseRatioAndStockIncreaseRatio (int year) {
+        try {
+            StrSQL = "select divd008,divd009/100,(divd006/10)+1,divd006,divd007 from divd_t where divd001 = ?divd001 and divd002 = ?divd002 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@divd001", no);
+            myCmd.Parameters.AddWithValue ("@divd002", year);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+
+                LastEPS = Double.Parse (reader.GetString (0)); //EPS-divd008
+                //EPSUseRatio = Double.Parse (reader.GetString (1)); //發放率-divd009
+                StockIncreaseRatio = Double.Parse (reader.GetString (2)); //股息-divd006
+                LastStockDividend = Double.Parse (reader.GetString (3)); //股息(股票)-divd006
+                LastMoneyDividend = Double.Parse (reader.GetString (4)); //股息(現金)-divd007
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+
+        try {
+            StrSQL = "select AVG(divd009)/100 from divd_t where divd001 = ?divd001 and divd002 >= ?divd002 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@divd001", no);
+            myCmd.Parameters.AddWithValue ("@divd002", year - 5);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                EPSUseRatio = Double.Parse (reader.GetString (0)); //五年內發放率-divd009
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+
+    public void getRevenueIncreaseRatio (int year) {
+        try {
+
+            StrSQL = "select (opme010/100)+1 from opme_t where opme001 = ?opme001 and opme002 = ?opme002  order by opme003 desc limit 1 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@opme001", no);
+            myCmd.Parameters.AddWithValue ("@opme002", year);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+
+                RevenueIncreaseRatio = Double.Parse (reader.GetString (0)); //EPS-opme010
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+
+    //五年平均EPS &　五年平均殖利率
+    public void getFiveYearsAvgerageEPSandDividend (int year) {
+        try {
+
+            StrSQL = "select ROUND(AVG(divd008),2),ROUND(AVG(divd010),2) from divd_t where divd001 = ?divd001 and divd002 between ?divd002s and ?divd002e ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@divd001", no);
+            myCmd.Parameters.AddWithValue ("@divd002s", year - 4); //起始年
+            myCmd.Parameters.AddWithValue ("@divd002e", year); //今年
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                FiveYearsAvgerageEPS = Double.Parse (reader.GetString (0)); //EPS-divd008
+                FiveYearsAvgerageDividend = Double.Parse (reader.GetString (1)); //EPS-divd010
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+
+    //近4季EPS
+    public void getLastFourEPS () {
+        try {
+
+            StrSQL = "select epsl004 from epsl_t,stck_t where epsl001 = stck001 and stck003 = 'Y' and epsl001 = ?epsl001 and epsl004 is not null and epsl003 <> 'ALL' order by epsl002 desc,epsl003 desc limit 4 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@epsl001", no);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                LastFourEPS = reader.GetString (0) + "|" + LastFourEPS; //EPS-epsl004
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+
+    //最後營收更新年月
+    public void getOperatingUpdateTime () {
+        try {
+
+            StrSQL = "select opme002,opme003 from opme_t where opme001=?opme001 order by opme002 desc,opme003 desc limit 1 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@opme001", no);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                OperatingUpdateTime = reader.GetString (0) + "/" + reader.GetString (1); //opme002(年)+opme003(月)
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+    }
+    public void getPriceAndUpdateDate () {
+
+        string type = "";
+
+        //先確定是上市(TWSE)或上櫃(TPEX)
+        try {
+
+            StrSQL = "select stck002 from stck_t where stck001 = ?stck001 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@stck001", no);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                type = reader.GetString (0); //類型-stck002
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+
+        if (type == "TWSE") {
+            //上市股票
+            try {
+                StrSQL = "select twse007,twse002 from twse_t where twse001 = ?twse001 and twse007 not like '%-%' order by twse002 desc limit 1 ";
+                MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+                myCmd.Parameters.AddWithValue ("@twse001", no);
+                MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+                while (reader.Read ()) {
+                    Price = Double.Parse (reader.GetString (0)); //收盤價-twse007
+                    UpdateDate = reader.GetString (1); //更新日-twse002
+                }
+                reader.Close ();
+
+            } catch (MySql.Data.MySqlClient.MySqlException ex) {
+                Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+            }
+        } else {
+            //上櫃股票
+            try {
+                StrSQL = "select tpex007,tpex002 from tpex_t where tpex001 = ?tpex001 and tpex007 not like '%-%' order by tpex002 desc limit 1 ";
+                MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+                myCmd.Parameters.AddWithValue ("@tpex001", no);
+                MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+                while (reader.Read ()) {
+                    Price = Double.Parse (reader.GetString (0)); //收盤價-twse007
+                    UpdateDate = reader.GetString (1); //更新日-twse002
+                }
+                reader.Close ();
+
+            } catch (MySql.Data.MySqlClient.MySqlException ex) {
+                Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+            }
+        }
+    }
+
+    public void getFiveYearsAvgeragePrice () {
+
+        string type = "";
+
+        //先確定是上市(TWSE)或上櫃(TPEX)
+        try {
+
+            StrSQL = "select stck002 from stck_t where stck001 = ?stck001 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@stck001", no);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                type = reader.GetString (0); //類型-stck002
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+            Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+        }
+
+        string prices = "";
+        if (type == "TWSE") {
+            //上市股票
+            try {
+                StrSQL = "select EXTRACT(YEAR FROM twse002),ROUND(avg(twse007),2) from twse_t where twse001 = ?twse001 group by EXTRACT(YEAR FROM twse002) order by EXTRACT(YEAR FROM twse002) desc limit 5";
+                MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+                myCmd.Parameters.AddWithValue ("@twse001", no);
+                MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+                while (reader.Read ()) {
+                    prices = reader.GetString (1) + "|" + prices; //均價
+                }
+                reader.Close ();
+
+            } catch (MySql.Data.MySqlClient.MySqlException ex) {
+                Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+            }
+        } else {
+            //上櫃股票
+            try {
+                StrSQL = "select EXTRACT(YEAR FROM tpex002),ROUND(avg(tpex007),2) from tpex_t where tpex001 = ?tpex001 group by EXTRACT(YEAR FROM tpex002) order by EXTRACT(YEAR FROM tpex002) desc limit 5 ";
+                MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+                myCmd.Parameters.AddWithValue ("@tpex001", no);
+                MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+                while (reader.Read ()) {
+                    prices = reader.GetString (1) + "|" + prices; //均價
+                }
+                reader.Close ();
+
+            } catch (MySql.Data.MySqlClient.MySqlException ex) {
+                Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+            }
+        }
+
+        FiveYearsAvgeragePrice = prices;
+    }
+
+    public void getType () {
+
+        //先確定是上市(TWSE)或上櫃(TPEX)
+        try {
+
+            StrSQL = "select IFNULL(stck005,'未知') from stck_t where stck001 = ?stck001 ";
+            MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+            myCmd.Parameters.AddWithValue ("@stck001", no);
+            MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+            while (reader.Read ()) {
+                Type = reader.GetString (0); //類型-stck005
+            }
+            reader.Close ();
+
+        } catch (MySql.Data.MySqlClient.MySqlException ex) {
+
+        }
+
+    }
+
+}
---
+++ ReportOfStock/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace ReportOfStock {
+    class Program {
+        static MySqlConnection conn;
+        static void Main (string[] args) {
+
+            //進行資料庫連線
+            database db = new database ();
+            conn = db.dbConect ();
+            //準備好檔案
+            string filePath = System.Environment.CurrentDirectory;
+
+            using (StreamWriter outputFile = new StreamWriter (Path.Combine (filePath, "RecommendedList.csv"))) {
+
+                Console.WriteLine ("檔案產出路徑:" + Path.Combine (filePath, "RecommendedList.csv"));
+
+                //產生時間
+                //outputFile.WriteLine ("產生時間:" + DateTime.Now);
+
+                string title = "編號,名稱,類別,去年EPS,五年平均EPS,近四季EPS,預測EPS,預測本益比,去年股利,去年股息,預測股利,五年平均殖利率,預測殖利率,合理價位,目前價格,五年均價,營收更新月,推(股利),推(EPS)";
+                outputFile.WriteLine (title);
+
+                //抓取資料並產出報表
+                string src;
+                if (args.Length == 0) {
+                    src = "stck_t";
+                } else {
+                    src = "stck_high_priority";
+                }
+
+                List<string> stockList = getList (src);
+                //stockList = new List<string> ();
+                //stockList.Add ("2820");
+
+                foreach (string stockNo in stockList) {
+                    Console.WriteLine ("開始運算..." + stockNo);
+                    parameters para = new parameters (stockNo, conn);
+                    //(當年淨利 / 當年累營收) / (去年淨利 / 去年營收) * 去年EPS * 累積年增率/ 股數增加率 = 當年度[預測]EPS
+                    double preEPS = Math.Round ((para.ThisYearAccumulationProfit / para.ThisYearAccumulationOperatingIncome) / (para.LastYearAccumulationProfit / para.LastYearAccumulationOperatingIncome) *
+                        para.LastEPS * para.RevenueIncreaseRatio / para.StockIncreaseRatio, 2);
+                    /*                                          Console.WriteLine ("ThisYearAccumulationProfit:" + para.ThisYearAccumulationProfit);
+                                                            Console.WriteLine ("ThisYearAccumulationOperatingIncome:" + para.ThisYearAccumulationOperatingIncome);
+                                                            Console.WriteLine ("LastYearAccumulationProfit:" + para.LastYearAccumulationProfit);
+                                                            Console.WriteLine ("LastYearAccumulationOperatingIncome:" + para.LastYearAccumulationOperatingIncome);
+                                                            Console.WriteLine ("LastEPS:" + para.LastEPS);
+                                                            Console.WriteLine ("RevenueIncreaseRatio:" + para.RevenueIncreaseRatio);
+                                                            Console.WriteLine ("StockIncreaseRatio:" + para.StockIncreaseRatio);
+                                                            Console.WriteLine ("preEPS:" + preEPS);
+                                          */
+                    //當年度[預測]EPS*股利發放率/7% = 合理價位
+
+                    double preDividend = Math.Round (preEPS * para.EPSUseRatio, 2);
+
+                    double goodPrice = Math.Round (preDividend / 0.07, 2);
+
+                    //預測本益比
+                    double preEPSRate = Math.Round (para.Price / preEPS, 2);
+
+                    //預測殖利率
+                    double preDividendRadio = Math.Round (preDividend / para.Price * 100, 2);
+
+                    //是否關注(by股利)
+                    string attention = "";
+                    //現價*1.2<目標價
+                    //預測EPS不超過五年平均*5
+                    //預測EPS>0
+                    if ((goodPrice / para.Price) > 1 && (preEPS / para.FiveYearsAvgerageEPS) < 5 && preEPS > 0) {
+                        attention = "Y";
+                    } else {
+                        attention = "N";
+                    }
+
+                    //是否關注(byEPS)
+                    string attention2 = "";
+                    //preESP大於現價10%
+                    //預測EPS不超過五年平均*5
+                    //預測EPS>0
+                    //近五年平均殖利率>3.5%
+                    //累積成長率每月平均達1%
+                    //發放率不大於100%
+                    double RevenueIncreaseRatioEveryMonth = (para.RevenueIncreaseRatio - 1) * 100 / DateTime.Now.Month;
+                    //Console.WriteLine(RevenueIncreaseRatioEveryMonth);
+                    //Console.WriteLine(para.RevenueIncreaseRatio);
+                    if ((para.Price / preEPS) < 15 &&
+                        (preEPS / para.FiveYearsAvgerageEPS) < 5 &&
+                        preEPS > 0 &&
+                        para.FiveYearsAvgerageDividend > 3.5 &&
+                        RevenueIncreaseRatioEveryMonth > 1 &&
+                        para.EPSUseRatio < 1
+                    ) {
+                        attention2 = "Y";
+                    } else {
+                        attention2 = "N";
+                    }
+
+                    //股價預測(股票編號,股票名稱,去年EPS,五年平均EPS,預測EPS,去年股利,預測股利,合理價位,目前價格)
+                    if (para.StockIncreaseRatio == 0) {
+                        //資料不完整, 沒有EPS, 該筆不處理
+                        Console.WriteLine ("此股資料不齊全:" + stockNo);
+                    } else {
+                        string content = stockNo + "," + //股票編號
+                            para.Name + "," + //股票名稱
+                            para.Type + "," + //股票類別
+                            para.LastEPS + "," + //去年EPS
+                            para.FiveYearsAvgerageEPS + "," + //五年平均EPS
+                            para.LastFourEPS + "," + //近四季EPS
+                            preEPS + "," + //預測EPS
+                            preEPSRate + "," + //預測本益比
+                            para.LastMoneyDividend + "," + //去年股利
+                            para.LastStockDividend + "," + //去年股息
+                            //para.RevenueIncreaseRatio + "," + //發放率
+                            preDividend + "," + //預測股利
+                            para.FiveYearsAvgerageDividend + "," + //五年平均殖利率
+                            preDividendRadio + "," + //預測殖利率
+                            goodPrice + "," + //合理價位
+                            para.Price + "," + //目前價格
+                            para.FiveYearsAvgeragePrice + "," + //五年均價
+                            para.OperatingUpdateTime + "," + //營收更新月
+                            attention + "," + //關注否(by股利)
+                            attention2; //關注否(by股利)
+                        //Console.WriteLine (content);
+                        outputFile.WriteLine (content);
+                    }
+                }
+            }
+        }
+
+        public static List<string> getList (string src) {
+            List<string> StockList = new List<string> ();
+
+            // 進行select (取出有股利資料 但無EPS資料的清單, 區間為當年份)
+            try {
+
+                //決定是否只找高優先的
+
+                string StrSQL = "select distinct stck001 from " + src + ",divd_t,opme_t,prft_t where stckstus='Y' and stck003='Y' " +
+                    " and stck001 = divd001 and divd002 = ?year " +
+                    " and stck001 = opme001 and opme002 = ?year " +
+                    " and stck001 = prft001 and opme002 = ?year " +
+                    " order by stck004,stck001";
+                //" and not exists ( select 1 from epsl_t where epsl001=stck001) ";
+                MySqlCommand myCmd = new MySqlCommand (StrSQL, conn);
+                myCmd.Parameters.AddWithValue ("@year", DateTime.Now.Year);
+                MySqlDataReader reader = myCmd.ExecuteReader (); //execure the reader
+                while (reader.Read ()) {
+                    String stockNo = reader.GetString (0);
+                    StockList.Add (stockNo);
+                    //Console.WriteLine(no);
+                }
+                reader.Close ();
+
+            } catch (MySql.Data.MySqlClient.MySqlException ex) {
+                Console.WriteLine ("Error2 " + ex.Number + " : " + ex.Message);
+            }
+
+            return StockList;
+        }
+    }
+}
